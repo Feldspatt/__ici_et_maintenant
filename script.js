bf2921b009a748348ce22f6369ba6455af315742
Ma <mathieuantoine4@gmail.com>
2023-11-10 22:08:44 +0100

    font asset

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 console.log('hello')
 
-// import { L, ResizeObserver} from 'leaflet'
-
 document.querySelectorAll('nav > span').forEach(a => {
-    // const s = document.createElement('span')
-    // s.setAttribute('data-href', a.href.split('#').pop())
-    // s.innerText = a.innerText
     a.addEventListener('click', (ev) => {
         ev.stopImmediatePropagation()
         Array.from(document.getElementsByClassName('active-link')).forEach(link => link.classList.remove('active-link'))
@@ -18,20 +13,19 @@ document.querySelectorAll('nav > span').forEach(a => {
         target.classList.add('targeted')
     })
 
-    // a.parentNode.replaceChild(s, a)
 })
 
-// window.addEventListener('load', ()=> {
-//     const map = L.map('map').setView([48.630841, -2.979158], 14);
-//     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//     }).addTo(map);
-//
-//     L.marker([48.630841, -2.979158]).addTo(map);
-//
-//     const resizeObserver = new ResizeObserver(() => {
-//         map.invalidateSize();
-//     });
-//
-//     resizeObserver.observe(document.getElementById('map'));
-// })
+window.addEventListener('load', ()=> {
+    const map = L.map('map').setView([48.630841, -2.979158], 14);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([48.630841, -2.979158]).addTo(map);
+
+    const resizeObserver = new ResizeObserver(() => {
+        map.invalidateSize();
+    });
+
+    resizeObserver.observe(document.getElementById('map'));
+})
